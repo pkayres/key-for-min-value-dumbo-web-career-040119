6fc748ff399b3055e8829fb0d5f29d416389e10e
Pete A <peter.ayresmusic@gmail.com>
2019-03-14 18:41:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,8 @@
 def key_for_min_value(name_hash)
   smallest_value = nil
   smallest_key = nil
+  if name_hash == nil
+    else 
   name_hash.each do |key,values|
         if values < smallest_value
         values = smallest_value
